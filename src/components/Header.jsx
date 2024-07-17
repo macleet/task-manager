c19c220fc15ebcc03b9911f3a3bf4c833e190003
Mac Lee <seemin0506@gmail.com>
2024-07-17 16:54:37 -0700

    Add Header component. Holds header text of app.

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+		<div className="flex w-full line items-center mb-6">
+    		<h1 className="text-4xl md:text-5xl lg:text-5xl" >task-manager</h1>
+		</div>
+	);
+}
+
+export default Header;
