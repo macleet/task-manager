315da962dd806c9df0f03d0199b0298acc7699d9
Mac Lee <seemin0506@gmail.com>
2024-07-17 17:16:58 -0700

    Add TaskAddField component.
    Text field where end users can input their tasks for items to show up
    on the tasks list above. When the end user presses Enter on their
    keyboard after entering a string of text, a POST request is sent to the
    server, where the database is updated with the relevant name, date,
    priorty, and folder of the task.

---
+++ src/components/TaskAddField.jsx
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+import { TextField } from '@mui/material';
+
+const TaskAddField = ({currFolder, setNewTask}) => {
+    const handleEnter = event => {
+        if (event.key !== 'Enter' || event.target.value === '') {
+            return;
+        }
+
+        const postTask = async name => {
+            try {
+                const response = await axios.post('http://localhost:8000/task/add', {
+                    "description": name,
+                    "date": null,
+                    "priority": false,
+                    "folder": currFolder
+                });
+                setNewTask(response.data[0]);
+            } catch (err) {
+                console.error(err.message);
+            }
+        };
+
+        postTask(event.target.value);
+        event.target.value = '';
+    }
+
+    return (
+		<TextField 
+            onKeyDown={handleEnter} 
+            placeholder="Press Enter↵ to add task..." 
+            size="small" 
+            className="w-full h-fit self-center"
+			autoComplete='off'
+        />
+    );
+};
+
+export default TaskAddField;
