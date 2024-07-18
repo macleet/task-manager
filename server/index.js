9074e591f57f5d78f36c6e2fe96a8286dfec411e
Mac Lee <seemin0506@gmail.com>
2024-07-18 13:42:10 -0700

    Add index.js. This Node.js app uses Express framework to create server to handle HTTP requests. Set up middleware (cors, json, url-encoded). Define routes (task, folder). Local server listens on port 8000 on run.

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import taskRouter from './task.js'
+import folderRouter from './folder.js'
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+/* Middlewares */
+app.use(cors());
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/task', taskRouter);
+app.use('/folder', folderRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server has started on port ${PORT}`);
+})
