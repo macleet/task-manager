89c52de8de2cc11722965b7675225d5bbd8c5b73
Mac Lee <seemin0506@gmail.com>
2024-08-06 15:13:08 -0700

    Remove unused props and rename state from currFolder to currFolderId.

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,21 @@ import ControlBar from './components/ControlBar/ControlBar.jsx';
 import TasksList from './components/TasksList/TasksList.jsx';
 import TaskAddField from './components/TaskAddField.jsx';
 
+import { TaskList } from './utilities/TaskList.js';
+
 import { useState } from 'react';
 
 function App() {
-	const [currFolder, setCurrFolder] = useState(1);
+    const [currFolderId, setCurrFolderId] = useState(1);
 	const [newTask, setNewTask] = useState();
     const [taskItems, setTaskItems] = useState([]);
 
   	return (
 		<div className="h-screen w-screen bg-blue-100 flex flex-col gap-3 justify-between p-6">
 			<Header />
-			<ControlBar taskItems={taskItems} setTaskItems={setTaskItems} currFolder={currFolder} setCurrFolder={setCurrFolder} />
-			<TasksList taskItems={taskItems} setTaskItems={setTaskItems} currFolder={currFolder} newTask={newTask} />
-			<TaskAddField currFolder={currFolder} setNewTask={setNewTask} />
+			<ControlBar currFolderId={currFolderId} setCurrFolderId={setCurrFolderId} />
+			<TasksList taskItems={taskItems} setTaskItems={setTaskItems} currFolderId={currFolderId} newTask={newTask} />
+			<TaskAddField currFolderId={currFolderId} setNewTask={setNewTask} />
     	</div>
   	);
 }
