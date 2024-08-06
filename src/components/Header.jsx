1f1c2e75cf88a9a0cc6111564c36f1321eff5978
Mac Lee <seemin0506@gmail.com>
2024-08-06 15:13:40 -0700

    Change header text.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
 	return (
 		<div className="flex h-fit pb-2 w-full line items-center">
-    		<h1 className="text-4xl md:text-5xl lg:text-5xl" >task-manager</h1>
+    		<h1 className="text-4xl md:text-5xl lg:text-5xl" >Task Manager</h1>
 		</div>
 	);
 }
