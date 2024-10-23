b9054baa0c5fc476953e57ce443a05087e765ba3
Mac Lee <seemin0506@gmail.com>
2024-10-22 22:33:39 -0700

    Delete Header.jsx for non-use.

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-	return (
-		<div className="flex h-fit pb-2 w-full line items-center">
-    		<h1 className="text-4xl md:text-5xl lg:text-5xl" >Task Manager</h1>
-		</div>
-	);
-}
-
-export default Header;
