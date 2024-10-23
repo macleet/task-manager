91066d7a7a8e777792fd8a8eac800d6f51a2f8ce
Mac Lee <seemin0506@gmail.com>
2024-10-22 22:32:14 -0700

    Change layout to omit Header and better integrate Timer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 import './styles/TaskManager.css';
 import './styles/Calendar.css';
 
-import Header from './components/Header.jsx';
 import ControlBar from './components/ControlBar/ControlBar.jsx';
 import TasksList from './components/TasksList/TasksList.jsx';
 import TaskAddField from './components/TaskAddField.jsx';
 
-import { TaskList } from './utilities/TaskList.js';
-
 import { useState } from 'react';
 
 function App() {
-    const [currFolderId, setCurrFolderId] = useState(1);
+	const [currFolderId, setCurrFolderId] = useState(1);
 	const [newTask, setNewTask] = useState();
     const [taskItems, setTaskItems] = useState([]);
+    const [searchItems, setSearchItems] = useState([]);
+	const [inputting, setInputting] = useState(false);
+    // const [folderMode, setFolderMode] = useState(false);
 
   	return (
-		<div className="h-screen w-screen bg-blue-100 flex flex-col gap-3 justify-between p-6">
-			<Header />
-			<ControlBar currFolderId={currFolderId} setCurrFolderId={setCurrFolderId} />
-			<TasksList taskItems={taskItems} setTaskItems={setTaskItems} currFolderId={currFolderId} newTask={newTask} />
-			<TaskAddField currFolderId={currFolderId} setNewTask={setNewTask} />
+		<div className="h-screen w-screen flex flex-col justify-start bg-blue-50">
+			<ControlBar setSearchItems={setSearchItems} inputting={inputting} setInputting={setInputting} setTaskItems={setTaskItems} currFolderId={currFolderId} setCurrFolderId={setCurrFolderId} />
+			<div className="flex flex-col justify-center h-full" >
+				<TasksList searchItems={searchItems} inputting={inputting} taskItems={taskItems} setTaskItems={setTaskItems} currFolderId={currFolderId} newTask={newTask} />
+				<TaskAddField currFolderId={currFolderId} setNewTask={setNewTask} />
+			</div>
     	</div>
   	);
 }
