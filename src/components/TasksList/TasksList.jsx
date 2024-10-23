65373e6de3c85f386e5aded2619e8b57d5d4503d
Mac Lee <seemin0506@gmail.com>
2024-10-22 22:57:59 -0700

    Change layout from flex list to grid for alignment and spacing control.

--- src/components/TasksList/TasksList.jsx
+++ src/components/TasksList/TasksList.jsx
@@ -1,37 +1,50 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 
 import TaskItem from './TaskItem';
 
 import axios from 'axios';
+import { TaskNode } from '../../utilities/TaskList';
 
-const TasksList = ({taskItems, setTaskItems, newTask, currFolder}) => {
-	const [buttonClicked, setButtonClicked] = useState(false);
-
+const TasksList = ({inputting, taskItems, searchItems, setTaskItems, newTask, currFolderId}) => {
     useEffect(() => {
         const getTasks = async () => {
 			try {
-				const response = await axios.get(`http://localhost:8000/task/getAll/${currFolder}`);
-				const taskItems = response.data.map(task => <TaskItem key={task.task_id} taskId={task.task_id} newTask={newTask} description={task.description} priority={task.priority} setButtonClicked={setButtonClicked} setTaskItems={setTaskItems} />);
+				const response = await axios.get(`http://localhost:8000/task/getAll/${currFolderId}`);
+				const taskItems = response.data.map((task) => new TaskNode(task.task_id, task.description, task.due_date, task.priority, task.folder_id));
 				setTaskItems(taskItems);
 			} catch (err) {
 				console.error(err.message);
 			}
         };
         getTasks();
-		setButtonClicked(false);
-    }, [currFolder, buttonClicked]);
+    }, [currFolderId]);
 
 	useEffect(() => {
 		if (newTask) {
-			const newTaskItem = <TaskItem key={newTask.task_id} taskId={newTask.task_id} newTask={newTask} description={newTask.description} priority={newTask.priority} setButtonClicked={setButtonClicked} setTaskItems={setTaskItems}  />; 
-			setTaskItems(taskItems => ([...taskItems, newTaskItem]));
+			const newTaskItem = {
+				task_id: newTask.task_id,
+				description: newTask.description,
+				due_date: newTask.due_date,
+				priority: newTask.priority,
+				folder_id: newTask.folder_id,
+			}; 
+			setTaskItems([...taskItems, newTaskItem]);
 		}
 	}, [newTask]);
+
+	const mapTasks = (items) => items.map((task) => <TaskItem key={task.task_id} due_date={new Date(task.due_date)} taskId={task.task_id} newTask={newTask} description={task.description} priority={task.priority} setTaskItems={setTaskItems} currFolderId={currFolderId} />);
 	
 	return (
-		<ul className="flex flex-col justify-start gap-4 h-full rounded-md bg-blue-300 shadow-md overflow-y-auto p-4 tasks" >
-			{taskItems}
-		</ul>
+		<div className="h-full flex flex-col justify-start gap-0.5 py-0.5 bg-blue-100 overflow-y-auto tasks" >
+			<div className="grid grid-cols-12 py-2 gap-y-0.5 text-gray-700 text-opacity-90 text-[15px]" >
+				<p className="pb-2 col-span-1 text-center" >Priority</p> 
+				<p className="pb-2 col-span-6" >Name</p>
+				<p className="pb-2 col-span-2 text-center" >Deadline</p>
+				<p className="pb-2 col-span-2 text-center" >Active Time</p>
+				<span className="pb-2 col-span-1" ></span>
+				{inputting ? mapTasks(searchItems) : mapTasks(taskItems)}
+			</div>
+		</div>
   	);
 };
 
