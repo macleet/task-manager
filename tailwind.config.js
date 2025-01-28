181f52325d2abc652bd65ed4393888be3574d714
Mac Lee <seemin0506@gmail.com>
2025-01-27 21:42:22 -0800

    tailwind config: Add custom inner shadow utility (inner-sm).

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{html,css,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "inner-sm": "inset 0 1px 2px 0 rgb(0 0 0 / 0.05)"
+      }
+    },
   },
   plugins: [],
 }
