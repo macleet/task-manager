dccaff667edcda33685ae144acf3535c05eabe37
Mac Lee <seemin0506@gmail.com>
2025-01-27 21:52:29 -0800

    feat: Implement folder management routes with CRUD
    - Add routes to get all folders and retrieve folder name by ID.
    - Implement functionality for adding, renaming, and deleting folders.
    - Ensure tasks are deleted when a folder is deleted.
    - Add error handling with appropriate responses for each route.

--- server/folder.js
+++ server/folder.js
@@ -1,11 +1,11 @@
-import pool from './db.js';
-import { Router } from 'express';
+import pool from "./db.js";
+import { Router } from "express";
 const folderRouter = Router();
 
 // Get all folders
-folderRouter.get('/', async (req, res) => {
+folderRouter.get("/", async (req, res) => {
     try {
-        const folders = await pool.query('SELECT * FROM folders');
+        const folders = await pool.query("SELECT * FROM folders ORDER BY folder_id ASC");
         res.json(folders.rows);
     } catch (err) {
         console.error(err.message);
@@ -13,9 +13,9 @@ folderRouter.get('/', async (req, res) => {
 });
 
 // Get folder name from folder id
-folderRouter.get('/:id', async (req, res) => {
+folderRouter.get("/:id", async (req, res) => {
     try {
-        const result = await pool.query('SELECT name FROM folders WHERE folder_id = $1', [req.params.id]);
+        const result = await pool.query("SELECT name FROM folders WHERE folder_id = $1", [req.params.id]);
         res.json(result.rows[0]);
     } catch (err) {
         console.error("get folder name error: ", err.message);
@@ -23,21 +23,21 @@ folderRouter.get('/:id', async (req, res) => {
 });
 
 // Add folder
-folderRouter.post('/add/:name', async (req, res) => {
+folderRouter.post("/add/:name", async (req, res) => {
     try {
-        const fid = await pool.query('INSERT INTO folders (name) VALUES ($1) RETURNING folder_id', [req.params.name]);
+        const fid = await pool.query("INSERT INTO folders (name) VALUES ($1) RETURNING folder_id", [req.params.name]);
         res.json({ id: fid });
     } catch (err) {
-        console.error("add folder error: ", err.message)
+        console.error("add folder error: ", err.message);
     }
 });
 
 // Change folder name
-folderRouter.patch('/change/:id', async (req, res) => {
+folderRouter.patch("/change/:id", async (req, res) => {
     try {
         const newName = req.body["name"];
-        await pool.query('SELECT name FROM folders WHERE folder_id = $1', [req.params.id]);
-        await pool.query('UPDATE folders SET name = $1 WHERE folder_id = $2', [newName, req.params.id]);
+        await pool.query("SELECT name FROM folders WHERE folder_id = $1", [req.params.id]);
+        await pool.query("UPDATE folders SET name = $1 WHERE folder_id = $2", [newName, req.params.id]);
         res.json();
     } catch (err) {
         console.error(err.message);
@@ -45,11 +45,11 @@ folderRouter.patch('/change/:id', async (req, res) => {
 });
 
 // Delete a folder
-folderRouter.delete('/delete/:id', async (req, res) => {
+folderRouter.delete("/delete/:id", async (req, res) => {
     try {
         const id = req.params.id;
-        const response = await pool.query('DELETE FROM folders WHERE folder_id = $1 RETURNING *', [id]);
-        await pool.query('DELETE FROM tasks WHERE folder_id = $1', [id]);
+        const response = await pool.query("DELETE FROM folders WHERE folder_id = $1 RETURNING *", [id]);
+        await pool.query("DELETE FROM tasks WHERE folder_id = $1", [id]);
         res.json(response.rows);
     } catch (err) {
         console.error(err.message);
