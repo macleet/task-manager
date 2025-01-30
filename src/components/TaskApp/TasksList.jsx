5d5615a1b32df7c7f0c5884bff6583c84771099a
Mac Lee <seemin0506@gmail.com>
2025-01-29 22:44:43 -0800

    feat: add container bottom state to handle context menu overflow on resize

--- src/components/TaskApp/TasksList.jsx
+++ src/components/TaskApp/TasksList.jsx
@@ -1,6 +1,8 @@
 import { 
 	useEffect,
+	useLayoutEffect,
 	useState,
+	useRef
 } from 'react';
 
 import TaskItem from './TaskItem.jsx';
@@ -9,6 +11,23 @@ import TaskAddField from './TaskAddField.jsx';
 import axios from 'axios';
 
 const TasksList = ({showFolder, searching, taskItems, searchItems, setTaskItems, currFolderId}) => {
+	// Task list container ref
+	const containerRef = useRef(null);
+
+	// State to store container bottom
+	const [bottom, setBottom] = useState(-1);
+
+	useLayoutEffect(() => {
+		setBottom(containerRef.current?.getBoundingClientRect().bottom);
+		const handleResize = () => setBottom(containerRef.current?.getBoundingClientRect().bottom);
+		window.addEventListener("resize", handleResize);
+
+		// Clean up
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		}
+	}, []);
+	
 	// State to store the new task details
 	const [newTask, setNewTask] = useState({
 		name: "",
@@ -86,10 +105,8 @@ const TasksList = ({showFolder, searching, taskItems, searchItems, setTaskItems,
 				notes={task.notes}
 				activeSeconds={task.activeSeconds}
 				tags={task.tags}
-				setTaskItems={setTaskItems} 
 				editTaskId={editTaskId} 
 				setEditTaskId={setEditTaskId}
-				currFolderId={currFolderId}
 				newTask={newTask} 
 				setTaskDeleted={setTaskDeleted}
 				parentBottom={bottom}
@@ -101,16 +118,20 @@ const TasksList = ({showFolder, searching, taskItems, searchItems, setTaskItems,
 			{/* Container for the task list */}
 			<div className={`flex flex-col relative z-10 shadow justify-start bg-blue-100 transition-all ${showFolder ? "w-4/5" : "w-full"}`} >
 				{/* Header row for task list */}
-				<div className="scroll grid grid-cols-12 py-3 gap-y-0.5 text-gray-700 text-opacity-90 text-[15px]" >
+				<div className="scroll grid grid-cols-[repeat(13,_minmax(0,_1fr))] py-3 gap-y-0.5 text-gray-700 text-opacity-90 text-[15px]" >
+					<p className="col-span-1 text-center text-[14px]" >Subtasks</p> 
 					<p className="col-span-1 text-center text-[14px]" >Priority</p> 
-					<p className="col-span-6 text-[14px]" >Name</p>
+					<p className="col-span-6 text-[14px]" 			  >Name</p>
 					<p className="col-span-2 text-center text-[14px]" >Deadline</p>
 					<p className="col-span-2 text-center text-[14px]" >Active Time</p>
-					<span className="col-span-1" ></span> {/* Spacer for action buttons */}
+					<p className="col-span-1 text-center text-[14px]" >Actions</p>
 				</div>
 
 				{/* Scrollable task list container */}
-				<div className="h-full scroll overflow-y-auto overflow-x-hidden" >
+				<div 
+					ref={containerRef}
+					className="h-full scroll overflow-y-scroll overflow-x-hidden"
+				>
 					{searching ? mapTasks(searchItems) : mapTasks(taskItems)}
 				</div>
 
