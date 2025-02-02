9f33d6d093ee5cd957c21f7a0e875a5202fad4b3
Mac Lee <seemin0506@gmail.com>
2025-02-02 14:39:04 -0800

    style fix: fix to properly display rounded container according to overflow

--- src/components/TaskApp/TaskMenu.jsx
+++ src/components/TaskApp/TaskMenu.jsx
@@ -35,17 +35,17 @@ const TaskMenu = ({taskId, editTaskId, setEditTaskId, hover, setDeleted, contain
     return (
         <div
             ref={menuContainerRef} 
-            className={`${hover ? "opacity-100" : "opacity-0 invisible scale-0"} ${isOverflowing ? "bottom-[25px]" : "top-[25px]" } flex flex-col gap-y-0.5 transition-opacity ease-in-out font-semibold text-slate-700 absolute right-[10px] bg-blue-200 z-50 rounded rounded-tr-none shadow`}
+            className={`${hover ? "opacity-100" : "opacity-0 invisible scale-0"} ${isOverflowing ? "bottom-[25px]" : "top-[25px]" } flex flex-col gap-y-0.5 transition-opacity ease-in-out font-semibold text-slate-700 absolute right-[10px] bg-blue-200 z-50 rounded ${isOverflowing ? "rounded-br-none" : "rounded-tr-none"}  shadow`}
         >
             <button 
                 onClick={handleEditClick} 
-                className="flex items-center justify-start text-left p-2 px-3 w-full hover:bg-blue-300 h-8 transition-colors ease-in-out rounded-tl"
+                className={`flex items-center justify-start text-left p-2 px-3 w-full hover:bg-blue-300 h-8 transition-colors ease-in-out ${isOverflowing ? "rounded-t" : "rounded-tl"}`}
             >
                 Edit
             </button>
             <button
                 onClick={handleDeleteClick}
-                className="flex items-center justify-start text-left p-2 px-3 w-full hover:bg-red-300 h-8 rounded-b transition-colors ease-in-out"
+                className={`flex items-center justify-start text-left p-2 px-3 w-full hover:bg-red-300 h-8  transition-colors ease-in-out ${isOverflowing ? "rounded-bl" : "rounded-b"}`}
             >
                 Delete
             </button>
