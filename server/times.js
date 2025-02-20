436f3be9adcf24e99e07fe742e671b591b695a97
Mac Lee <seemin0506@gmail.com>
2025-02-20 03:34:35 -0800

    Add get request route for current active task

--- server/times.js
+++ server/times.js
@@ -2,16 +2,23 @@ import pool from "./db.js";
 import { Router } from "express";
 const timesRouter = Router();
 
-timesRouter.get("/", async (req, res) => {
+timesRouter.get("/getActive", async (req, res) => {
+    try {
+        const { taskId } = req.query;
+        const results = await pool.query("SELECT active FROM times WHERE task_id = $1", [taskId]);
+        const { active } = results.rows[0];
+        res.json({ active });
+    } catch (error) {
+        console.error("Error getting active boolean for task", error);
+    }
+});
+
+timesRouter.get("/getElapsedMinutes", async (req, res) => {
     try {
         const { taskId } = req.query;
         const results = await pool.query("SELECT elapsed_minutes, active FROM times WHERE task_id = $1", [taskId]);
-        console.log(results.rows)
-        const { elapsed_minutes: elapsedMinutes, active } = results.rows[0];
-        res.json({ 
-            elapsedMinutes: elapsedMinutes,
-            active: active
-        });
+        const { elapsed_minutes: elapsedMinutes } = results.rows[0];
+        res.json({ elapsedMinutes });
     } catch (error) {
         console.error("Error getting elapsed time for task", error);
     }
@@ -21,9 +28,7 @@ timesRouter.patch("/setActive", async (req, res) => {
     try {
         const { taskId, isActive } = req.body;
         await pool.query("UPDATE times SET active = $1", [false]);
-        if (isActive) {
-            await pool.query("UPDATE times SET active = $1 WHERE task_id = $2", [true, taskId]);
-        }
+        await pool.query("UPDATE times SET active = $1 WHERE task_id = $2", [isActive, taskId]);
         res.json();
     } catch (error) {
         console.error("Error setting active task for timer", error);
--- src/components/TaskItem/ActiveTimer.jsx
+++ src/components/TaskItem/ActiveTimer.jsx
@@ -3,25 +3,35 @@ import { useState, useEffect } from "react";
 import { useActiveTaskContext } from "../../context/ActiveTaskContext";
 
 export default ({taskId, editTaskId}) => {
-    const {setActiveTaskId} = useActiveTaskContext();
+    const {activeTaskId, setActiveTaskId} = useActiveTaskContext();
     const [isActive, setIsActive] = useState(false);
     const [activeTime, setActiveTime] = useState(0);
 
     useEffect(() => {
-        const getElapsedTime = async () => {
+        const initializeTime = async () => {
             try {
-                const response = await axios.get("https://task-manager-server-6eht.onrender.com/times", {
+                const response = await axios.get("https://task-manager-server-6eht.onrender.com/times/getElapsedMinutes", {
                     params: {
                         taskId: taskId
                     }
                 });
                 setIsActive(response.data.active);
-                setActiveTime(response.data.elapsedMinutes);
             } catch (error) {
                 console.error("Error fetching elapsed time", error);
             }
+
+            try {
+                const response = await axios.get("https://task-manager-server-6eht.onrender.com/times/getActive", {
+                    params: {
+                        taskId: taskId
+                    }
+                });
+                setActiveTime(response.data.elapsedMinutes);
+            } catch (error) {
+                console.error("Error fetching current active task", error);
+            }
         };
-        getElapsedTime();
+        initializeTime();
     }, []);
 
     const toggleActiveTask = async (event) => {
@@ -30,7 +40,7 @@ export default ({taskId, editTaskId}) => {
                 taskId: taskId,
                 isActive: event.target.checked
             });
-            setIsActive(event.target.checked);
+            // setIsActive(event.target.checked);
             if (event.target.checked) setActiveTaskId(taskId);
             else setActiveTaskId(-1);
         } catch (error) {
@@ -41,7 +51,7 @@ export default ({taskId, editTaskId}) => {
     return(
         <div className={`h-16 col-span-2 flex justify-center items-center gap-2 text-black bg-opacity-40 transition-all ${editTaskId === taskId ? "bg-blue-400" : "bg-white"}`} >
             <p>{activeTime}</p>
-            <input value={isActive} onChange={toggleActiveTask} type="checkbox" />
+            <input checked={isActive} onChange={toggleActiveTask} type="checkbox" />
         </div>
     );
 }
