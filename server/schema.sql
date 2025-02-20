7a7a7485bfd081f6c5f7d600c538f87feb55522d
Mac Lee <seemin0506@gmail.com>
2025-02-20 01:31:55 -0800

    Change schema to have  times table to include rested minutes for stats

--- server/schema.sql
+++ server/schema.sql
@@ -18,7 +18,8 @@ CREATE TABLE IF NOT EXISTS times(
     time_id SERIAL PRIMARY KEY,
     task_id INTEGER REFERENCES tasks(task_id) ON DELETE CASCADE,
     elapsed_minutes INTEGER DEFAULT 0,
-    active BOOLEAN DEFAULT FALSE
+    active BOOLEAN DEFAULT FALSE,
+    rested_minutes INTEGER DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS phases (
