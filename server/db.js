56081a770992ed6425b000e93015bc89bdb98afb
Mac Lee <seemin0506@gmail.com>
2025-02-19 18:46:35 -0800

    Change to use internal database URL

--- server/db.js
+++ server/db.js
@@ -4,11 +4,7 @@ import pg from 'pg';
 import path from 'path';
 
 const pool = new pg.Pool({
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOSTNAME,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB
+    connectionString: process.env.DATABASE_URL
 });
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
