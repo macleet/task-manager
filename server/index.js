0a78594ebff7607019930591ce50d94f5dc261b5
Mac Lee <seemin0506@gmail.com>
2025-02-20 01:37:25 -0800

    Add times route for task elapsed time retrieval and toggling current active timer task

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import taskRouter from "./task.js";
 import subtaskRouter from "./subtask.js";
 import folderRouter from "./folder.js";
+import timesRouter from "./times.js";
 import cors from "cors";
 import express from "express";
 
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/task", taskRouter);
 app.use("/subtask", subtaskRouter);
 app.use("/folder", folderRouter);
+app.use("/times", timesRouter);
 
 app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`);
---
+++ server/times.js
@@ -0,0 +1,32 @@
+import pool from "./db.js";
+import { Router } from "express";
+const timesRouter = Router();
+
+timesRouter.get("/", async (req, res) => {
+    try {
+        const { taskId } = req.query;
+        const results = await pool.query("SELECT elapsed_minutes, active FROM times WHERE task_id = $1", [taskId]);
+        const { elapsed_minutes: elapsedMinutes, active } = results.rows[0];
+        res.json({ 
+            elapsedMinutes: elapsedMinutes,
+            active: active
+        });
+    } catch (error) {
+        console.error("Error getting elapsed time for task", error);
+    }
+});
+
+timesRouter.put("/setActive", async (req, res) => {
+    try {
+        const { taskId, isActive } = req.query;
+        await pool.query("UPDATE times SET active = $1", [false]);
+        if (isActive) {
+            await pool.query("UPDATE times SET active = $1 WHERE task_id = $2", [true, taskId]);
+        }
+        res.json();
+    } catch (error) {
+        console.error("Error setting active task for timer", error);
+    }
+})
+
+export default timesRouter;
