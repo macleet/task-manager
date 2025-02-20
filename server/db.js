c49a7dbba92dd6fda8d1a4478e3137d166b4695c
Mac Lee <seemin0506@gmail.com>
2025-02-19 17:53:54 -0800

    Configure postgres database to use schema.sql to create tasks, folders, phases, and steps tables, and insert Main folder

--- server/db.js
+++ server/db.js
@@ -1,12 +1,32 @@
-import 'dotenv/config'
+import 'dotenv/config';
+import fs from 'fs';
 import pg from 'pg';
+import path from 'path';
 
 const pool = new pg.Pool({
-    user: 'postgres',
-    password: process.env.POSTGRES_PASS,
-    host: 'localhost',
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: "db",
     port: 5432,
-    database: 'task_manager_v2'
+    database: process.env.POSTGRES_DB
 });
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const schemaSql = fs.readFileSync(path.join(__dirname, 'schema.sql'), 'utf8');
+
+async function initializeDb() {
+    try {
+        await pool.connect();  // Ensure we are connected to the database
+        console.log('Connected to the database');
+
+        // Run the schema SQL to create the tables
+        await pool.query(schemaSql);
+        console.log('Database schema created successfully');
+    } catch (err) {
+        console.error('Error running schema.sql', err);
+    }
+}
+  
+initializeDb().catch((err) => console.error(err));
 
 export default pool;
---
+++ server/schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS tasks (
+    task_id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    due_date VARCHAR(32) DEFAULT '',
+    priority INTEGER DEFAULT 1,
+    folder_id INTEGER,
+    notes VARCHAR(500) DEFAULT '',
+    active_seconds INTEGER DEFAULT 0,
+    tags VARCHAR(20)[] DEFAULT '{}'::VARCHAR[]
+);
+
+CREATE TABLE IF NOT EXISTS folders(
+    folder_id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS phases (
+    phase_id SERIAL PRIMARY KEY,
+    task_id INTEGER REFERENCES tasks(task_id) ON DELETE CASCADE,
+    phase_name VARCHAR(255) NOT NULL,
+    phase_description TEXT,
+    completed BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS steps (
+    step_id SERIAL PRIMARY KEY,
+    step_name VARCHAR(255) NOT NULL,
+    step_description TEXT,
+    estimated_time INTEGER,
+    completed BOOLEAN DEFAULT FALSE,
+    phase_id INTEGER NOT NULL,
+    CONSTRAINT fk_phase FOREIGN KEY (phase_id) REFERENCES phases(phase_id) ON DELETE CASCADE
+);
+
+INSERT INTO folders (name)
+SELECT "Main" WHERE NOT EXISTS (SELECT 1 FROM folders WHERE name = "Main");
