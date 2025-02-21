e2cba13bb2b1f7692cbfd24ce40fe98567e23d5b
Mac Lee <seemin0506@gmail.com>
2025-02-20 16:26:48 -0800

    cleanup: Use JSON object property shorthand for cleaner code.

--- server/times.js
+++ server/times.js
@@ -17,8 +17,8 @@ timesRouter.get("/getElapsedMinutes", async (req, res) => {
     try {
         const { taskId } = req.query;
         const results = await pool.query("SELECT elapsed_minutes, active FROM times WHERE task_id = $1", [taskId]);
-        const { elapsed_minutes: elapsedMinutes } = results.rows[0];
-        res.json({ elapsedMinutes: elapsedMinutes });
+        const { elapsed_minutes: elapsedMinutes, active } = results.rows[0];
+        res.json({ elapsedMinutes, active });
     } catch (error) {
         console.error("Error getting elapsed time for task", error);
     }
