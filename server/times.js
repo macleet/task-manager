e4c01ef45a1f442ba0f6df0458f150d1aedbd6b0
Mac Lee <seemin0506@gmail.com>
2025-02-21 04:08:58 -0800

    fix: fix to properly compare db date and current date with new Date instance when setting elapsed and rested minutes

--- server/times.js
+++ server/times.js
@@ -55,8 +55,9 @@ timesRouter.patch("/setElapsedMinutes", async (req, res) => {
     try {
         const { taskId, elapsedTime } = req.body;
         const result = await pool.query("SELECT elapsed_minutes, date FROM times WHERE task_id = $1", [taskId]);
-        const { elapsed_minutes: currentMinutes, date } = result.rows[0];
-        if (date !== currentDate) {
+        const { elapsed_minutes: currentMinutes, date: dbDate } = result.rows[0];
+        const date = new Date(dbDate);
+        if (date < currentDate) {
             await pool.query("INSERT INTO times (task_id, date, elapsed_time) VALUES ($1, $2, $3)", [taskId, currentDate, elapsedTime]);
         } else {
             await pool.query("UPDATE times SET elapsed_minutes = $1 WHERE task_id = $2 AND date = $3", [elapsedTime + currentMinutes, taskId, date]);
@@ -73,8 +74,9 @@ timesRouter.patch("/setRestedMinutes", async (req, res) => {
     try {
         const { taskId, elapsedTime } = req.body;
         const result = await pool.query("SELECT rested_minutes, date FROM times WHERE task_id = $1", [taskId]);
-        const { rested_minutes: currentMinutes, date } = result.rows[0];
-        if (date !== currentDate) {
+        const { rested_minutes: currentMinutes, date: dbDate } = result.rows[0];
+        const date = new Date(dbDate);
+        if (date < currentDate) {
             await pool.query("INSERT INTO times (task_id, date, rested_time) VALUES ($1, $2, $3)", [taskId, currentDate, elapsedTime]);
         } else {
             await pool.query("UPDATE times SET rested_minutes = $1 WHERE task_id = $2 AND date = $3", [elapsedTime + currentMinutes, taskId, date]);
