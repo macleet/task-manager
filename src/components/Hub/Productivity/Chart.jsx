5e37e18584742e42da46cba76e5055bd0c8478f6
Mac Lee <seemin0506@gmail.com>
2025-02-23 23:47:00 -0800

    refactor: move chart options, label, and datasets to constants/chart.js for better readability in Chart component

--- src/components/Hub/Productivity/Chart.jsx
+++ src/components/Hub/Productivity/Chart.jsx
@@ -11,8 +11,9 @@ import {
     Tooltip, 
     Legend 
 } from 'chart.js';
-import axios from "axios";
 import { useTimerContext } from "../../../context/TimerContext";
+import { getGraphData } from "../../../utilities/api";
+import { options, daysLabel, restedDataset, workedDataset } from "../../../constants/chart";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
@@ -21,120 +22,20 @@ export default ({ taskId }) => {
     const [weekPeriod, setWeekPeriod] = useState(new WeekPeriod());
 
     const [chartData, setChartData] = useState({
-        labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-        datasets: [
-            {
-                label: 'Rested',
-                data: [],
-                backgroundColor: '#547bc9',
-                borderRadius: 2,
-            },
-            {
-                label: 'Worked',
-                data: [],
-                backgroundColor: 'rgb(204, 162, 126)',
-                borderRadius: 2,
-            },
-        ],
+        labels: daysLabel,
+        datasets: [restedDataset, workedDataset]
     });
-
-    const options = {
-        responsive: true,
-        maintainAspectRatio: false,
-        layout: {
-            backgroundColor: '#f0f0f0',
-            padding: {
-                top: 15,
-                left: 20, 
-                right: 20,
-            },
-        },
-        plugins: {
-            legend: {
-                position: 'top',
-                labels: {
-                    font: {
-                        size: 13, 
-                        weight: 600,
-                    },
-                    pointStyle: 'rectRounded',
-                    usePointStyle: true,
-                    color: "#333b4b",
-                },
-            },
-        },
-        scales: {
-            x: {
-                stacked: true, 
-                grid: {
-                    display: false,
-                },
-                ticks: {
-                    font: {
-                        size: 11.5,
-                        weight: 600,
-                    },
-                    color: "#333b4b",
-                },
-            },
-            y: {
-                grid: {
-                    display: false,
-                    color: "rgb(204, 162, 126)",
-                },
-                ticks: {
-                    font: {
-                        size: 11,
-                        weight: 600,
-                    },
-                    maxTicksLimit: 6,
-                    color: "#333b4b",
-                },
-                beginAtZero: true,
-                title: {
-                    display: true,
-                    text: "Hours",
-                    padding: {
-                        bottom: 10
-                    }
-                }
-            },
-        },
-    };
 
     useEffect(() => {
         if (!paused) return;
-        const getGraphData = async () => {
-            try {
-                const response = await axios.get("https://task-manager-server-6eht.onrender.com/times/getChartData", {
-                    params: {
-                        taskId: taskId,
-                        periodDates: weekPeriod.getPeriodDatesArray()
-                    }
-                });
-                const { activeData, restData } = response.data;
-                setChartData({
-                    labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-                    datasets: [
-                        {
-                            label: 'Rested',
-                            data: restData,
-                            backgroundColor: '#547bc9',
-                            borderRadius: 2,
-                        },
-                        {
-                            label: 'Worked',
-                            data: activeData,
-                            backgroundColor: 'rgb(204, 162, 126)',
-                            borderRadius: 2,
-                        },
-                    ],
-                });
-            } catch (error) {
-               console.error("GET request error for retrieving graph data", error);
-            }
+        const getGraphDataFromApi = async () => {
+            const { activeData, restData } = await getGraphData(taskId, weekPeriod.getPeriodDatesArray());
+            setChartData(prev => ({
+                ...prev,
+                datasets: [{...restedDataset, data: restData}, {...workedDataset, data: activeData}]
+            }));
         };
-        getGraphData();
+        getGraphDataFromApi();
     }, [weekPeriod, paused]);
 
     return(
---
+++ src/constants/chart.js
@@ -0,0 +1,79 @@
+export const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    layout: {
+        backgroundColor: '#f0f0f0',
+        padding: {
+            top: 15,
+            left: 20, 
+            right: 20,
+        },
+    },
+    plugins: {
+        legend: {
+            position: 'top',
+            labels: {
+                font: {
+                    size: 13, 
+                    weight: 600,
+                },
+                pointStyle: 'rectRounded',
+                usePointStyle: true,
+                color: "#333b4b",
+            },
+        },
+    },
+    scales: {
+        x: {
+            stacked: true, 
+            grid: {
+                display: false,
+            },
+            ticks: {
+                font: {
+                    size: 11.5,
+                    weight: 600,
+                },
+                color: "#333b4b",
+            },
+        },
+        y: {
+            grid: {
+                display: false,
+                color: "rgb(204, 162, 126)",
+            },
+            ticks: {
+                font: {
+                    size: 11,
+                    weight: 600,
+                },
+                maxTicksLimit: 6,
+                color: "#333b4b",
+            },
+            beginAtZero: true,
+            title: {
+                display: true,
+                text: "Hours",
+                padding: {
+                    bottom: 10
+                }
+            }
+        },
+    },
+};
+
+export const restedDataset = {
+    label: 'Rested',
+    data: [],
+    backgroundColor: '#547bc9',
+    borderRadius: 2,
+};
+
+export const workedDataset = {
+    label: 'Worked',
+    data: [],
+    backgroundColor: 'rgb(204, 162, 126)',
+    borderRadius: 2,
+};
+
+export const daysLabel = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
