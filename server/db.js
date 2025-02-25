6bc4ccfe06c9e53c5e6a59304d4cc20f046eace8
Mac Lee <seemin0506@gmail.com>
2025-02-24 19:15:09 -0800

    chore: change single quote to double quotes for consistency

--- server/db.js
+++ server/db.js
@@ -1,8 +1,8 @@
-import 'dotenv/config';
-import fs from 'fs';
-import pg from 'pg';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import "dotenv/config";
+import fs from "fs";
+import pg from "pg";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const isDevelopment = process.env.DEVELOPMENT === "true";
 
@@ -15,18 +15,18 @@ const pool = new pg.Pool({
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const schemaSql = fs.readFileSync(path.join(__dirname, 'schema.sql'), 'utf8');
+const schemaSql = fs.readFileSync(path.join(__dirname, "schema.sql"), "utf8");
 
 async function initializeDb() {
     try {
         await pool.connect();  // Ensure we are connected to the database
-        console.log('Connected to the database');
+        console.log("Connected to the database");
 
         // Run the schema SQL to create the tables
         await pool.query(schemaSql);
-        console.log('Database schema created successfully');
+        console.log("Database schema created successfully");
     } catch (err) {
-        console.error('Error running schema.sql', err);
+        console.error("Error running schema.sql", err);
     }
 }
   
