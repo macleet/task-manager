06c2bd8bc6ead814f8c5131f91fe3f328a1369d0
Mac Lee <seemin0506@gmail.com>
2025-02-26 02:34:43 -0800

    fix: Remove condition to update chart if the task wherein time is being added is the active one

--- src/components/Hub/Productivity/Chart.jsx
+++ src/components/Hub/Productivity/Chart.jsx
@@ -2,26 +2,16 @@ import WeekPeriod from "../../../utilities/WeekPeriod";
 import WeekNavigator from "./WeekNavigator";
 import { useEffect, useState, useRef } from "react";
 import { Bar } from "react-chartjs-2";
-import { 
-    Chart as ChartJS, 
-    CategoryScale, 
-    LinearScale, 
-    BarElement, 
-    Title, 
-    Tooltip, 
-    Legend 
-} from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { useTimerContext } from "../../../context/TimerContext";
 import { getGraphData } from "../../../utilities/api";
 import { options, daysLabel, restedDataset, workedDataset } from "../../../constants/chart";
-import { useActiveTaskContext } from "../../../context/ActiveTaskContext";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export default ({ taskId }) => {
     const chartRef = useRef(null);
     const { paused } = useTimerContext();
-    const { activeTaskId } = useActiveTaskContext();
     const [weekPeriod, setWeekPeriod] = useState(new WeekPeriod());
     const [chartData, setChartData] = useState({
         labels: daysLabel,
@@ -34,20 +24,19 @@ export default ({ taskId }) => {
             ...prev,
             datasets: [{...restedDataset, data: restData}, {...workedDataset, data: activeData}]
         }));
-        chartRef.current?.update();
     };
 
     useEffect(() => { getGraphDataFromApi() }, []);
     useEffect(() => {
-        if (!paused || taskId !== activeTaskId) return;
+        if (!paused) return;
         getGraphDataFromApi();
+        chartRef.current?.update();
     }, [weekPeriod, paused]);
 
     return(
         <div className="flex flex-col bg-blue-200 bg-opacity-40 rounded-xl w-full shadow-sm" >
             <div className="h-[280px] w-full">
                 <Bar
-                    // redraw={true}
                     ref={chartRef}
                     data={chartData}
                     options={options}
